12e2a52e9bf8e34f2cba1587551c58b8e8583fb5
Pedro J. Pérez <pedro.j.perez@uv.es>
2021-04-12 20:54:14 +0200

    plantillas

--- R/pjp_template_bitacora.R
+++ R/pjp_template_bitacora.R
@@ -1,19 +1,15 @@
-#' @title Call and open la plantilla para crear una bitacora
-#' @description Usar la plantilla para bitacoras y empezar a documentar el Rproject
+#' @title call and open la plantilla para crear una bitacora
+#' @description Uuar la plantilla para bitacoras y empezar a documentar el Rproject
+#'
 #' @param name name of your TidyTuesday analysis file
-#' @param open should the file be opened after being created
 #' @importFrom usethis use_template
 #' @importFrom fs dir_create
 #' @examples
-#' \donttest{
-#' if(interactive()){
-#'   use_tidytemplate(name = "My_Awesome_TidyTuesday.Rmd")
-#' }
-#' }
-#'
+#' pjp_template_bitacora()
 #' @export
-pjp_template_bitacora <-
-  function(name = NULL, open = interactive(), ... ) {
+
+
+pjp_template_bitacora <- function(name = NULL, open = interactive(), ... ) {
   use_template("bitacora.Rmd", save_as = "bitacora.Rmd",
                package = "pjpv2020.01", ... , open = open)
   fs::dir_create("./datos/")
