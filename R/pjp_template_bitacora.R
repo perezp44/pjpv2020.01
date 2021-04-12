53f8f729f90375b8e1d11af191945c1d0b117b89
Pedro J. Pérez <pedro.j.perez@uv.es>
2021-04-12 21:00:44 +0200

    plantillas

--- R/pjp_template_bitacora.R
+++ R/pjp_template_bitacora.R
@@ -2,6 +2,7 @@
 #' @description Uuar la plantilla para bitacoras y empezar a documentar el Rproject
 #'
 #' @param name name of your TidyTuesday analysis file
+#' @param open si estas en interactivo ono
 #' @importFrom usethis use_template
 #' @importFrom fs dir_create
 #' @examples
@@ -9,14 +10,11 @@
 #' @export
 
 
-pjp_template_bitacora <- function(name = NULL, open = interactive(), ... ) {
-  use_template("bitacora.Rmd", save_as = "bitacora.Rmd",
-               package = "pjpv2020.01", ... , open = open)
+pjp_template_bitacora <- function(name = NULL, open = interactive() ){
+  use_template("bitacora.Rmd", save_as = "bitacora.Rmd", package = "pjpv2020.01", ... , open = open)
   fs::dir_create("./datos/")
   fs::dir_create("./imagenes/")
   fs::dir_create("./assets/")
-  use_template("bitacora.css", save_as = "./assets/bitacora.css",
-               package = "pjpv2020.01", ..., open = FALSE)
-  use_template("_script.R", save_as = "./_script.R",
-               package = "pjpv2020.01", ..., open = FALSE)
+  use_template("bitacora.css", save_as = "./assets/bitacora.css", package = "pjpv2020.01", ..., open = FALSE)
+  use_template("_script.R", save_as = "./_script.R", package = "pjpv2020.01", ..., open = FALSE)
 }
